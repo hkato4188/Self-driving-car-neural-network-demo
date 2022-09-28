86a5711c50c340dccbef21d81247d8345e9fff06
hkato4188 <hirokikato1@gmail.com>
2022-09-28 18:39:04 -0400

    create friction, max speed, acceleration, and other update features for car class

--- car.js
+++ car.js
@@ -1,35 +1,57 @@
 class Car {
-    constructor(x,y,width,height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.controls=new Controls();
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = 0;
+    this.acceleration = 0.1;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if(Math.abs(this.speed) < this.friction){
+        this.speed = 0;
     }
 
-    update(){
-        if(this.controls.forward){
-            this.y-=2;
-        }
-        if(this.controls.reverse){
-            this.y+=2;
-        }
-        if(this.controls.left){
-            this.x-=2;
-        }
-        if(this.controls.right){
-            this.x+=2;
-        }
+    this.y -= this.speed;
+    
+    if (this.controls.left) {
+      this.x -= 2;
     }
+    if (this.controls.right) {
+      this.x += 2;
+    }
+  }
 
-    draw(ctx){
-        ctx.beginPath();
-        ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
-    }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
 }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const canvas=document.getElementById("myCanvas");
-canvas.height=window.innerHeight;
+
 canvas.width=200;
 
 const ctx = canvas.getContext("2d");
@@ -9,6 +9,7 @@ car.draw(ctx);
 animate();
 function animate(){
     car.update();
+    canvas.height=window.innerHeight;
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
