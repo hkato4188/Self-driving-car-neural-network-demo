d4e58bacccb3700e584465b171b3e94efbe7a2f3
hkato4188 <hirokikato1@gmail.com>
2022-11-04 15:36:59 -0400

    create lerp function linear interpolation for lanes on road class

--- road.js
+++ road.js
@@ -6,6 +6,7 @@ class Road{
 
         this.left=x-width/2;
         this.right=x+width/2;
+        
         const infinity=1000000;
         this.top=-infinity;
         this.bottom=infinity;
@@ -15,14 +16,20 @@ class Road{
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
+        for(let i=0; i<=this.laneCount;i ++){
+            const x=lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+                );
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
+        }
     }
+}
+
+function lerp(A,B,t){
+    return A+(B-A)*t;
 }
