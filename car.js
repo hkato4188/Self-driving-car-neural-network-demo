bffbe07387729ae335c827bbe9022d2b40427d0b
hkato4188 <hirokikato1@gmail.com>
2022-11-04 12:36:47 -0400

    update left and right control steering directions

--- car.js
+++ car.js
@@ -5,6 +5,7 @@ class Car {
     this.width = width;
     this.height = height;
     this.speed = 0;
+    this.angle = 0;
     this.acceleration = 0.1;
     this.maxSpeed = 3;
     this.friction = 0.05;
@@ -34,24 +35,33 @@ class Car {
         this.speed = 0;
     }
 
-    this.y -= this.speed;
+    if(this.speed!=0){
+      const flip=this.speed>0?1:-1;
+      if (this.controls.left) {
+        this.angle+=0.03*flip;
+      }
+      if (this.controls.right) {
+        this.angle-=0.03*flip;
+      }
+    }
     
-    if (this.controls.left) {
-      this.x -= 2;
-    }
-    if (this.controls.right) {
-      this.x += 2;
-    }
+    this.x-=Math.sin(this.angle)*this.speed;
+    this.y-=Math.cos(this.angle)*this.speed;
+    
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
     ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
+      -this.width/2,
+      -this.height/2,
       this.width,
       this.height
     );
     ctx.fill();
+    ctx.restore();
   }
 }
