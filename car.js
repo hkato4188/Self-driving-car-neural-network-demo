cb979d1639ffdd9ba2e0895a6bb47f298875f15c
hkato4188 <hirokikato1@gmail.com>
2022-11-09 11:21:29 -0500

    update canvas element by creating a network and car cavnas

--- car.js
+++ car.js
@@ -10,13 +10,19 @@ class Car {
     this.acceleration = 0.2;
     this.maxSpeed = maxSpeed;
     this.friction = 0.05;
-    this.controls = new Controls(controlType);
+    
 
     this.angle = 0;
+    this.damaged = false;
+    this.useBrain = controlType=="AI";
+
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain=new NeuralNetwork(
+        [this.sensor.rayCount,6,4]
+      );
     }
-    this.damaged = false;
+    this.controls = new Controls(controlType);
   }
 
   update(roadBorders, traffic) {
@@ -27,6 +33,18 @@ class Car {
     }
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets=this.sensor.readings.map(
+        s => s == null ? 0 : 1 - s.offset
+      );
+      const outputs = NeuralNetwork.feedForward(offsets,this.brain);
+      
+
+      if(this.useBrain){
+        this.controls.forward=outputs[0];
+        this.controls.left=outputs[1];
+        this.controls.right=outputs[2];
+        this.controls.reverse=outputs[3];
+      }
     }
   }
 
