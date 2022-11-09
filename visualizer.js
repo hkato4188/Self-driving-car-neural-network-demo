6fba435b562621534f30d2729ab0ebdb7ee182e5
hkato4188 <hirokikato1@gmail.com>
2022-11-09 13:09:35 -0500

    create drawLevel method for visualizer class connecting each level input node to output node highlighting bias

--- visualizer.js
+++ visualizer.js
@@ -15,33 +15,67 @@ class Visualizer{
         const right = left+width;
         const bottom=top+height;
         
-        const {inputs,outputs}=level;
+        const {inputs,outputs,weights,biases}=level;
 
+        
+        for(let i=0; i<inputs.length; i++){
+            for(let j=0; j<outputs.length; j++){
+                ctx.beginPath();
+                ctx.moveTo(
+                    Visualizer.#getNodeX(inputs,i,left,right),
+                    bottom
+                );
+                ctx.lineTo(
+                    Visualizer.#getNodeX(outputs,j,left,right),
+                    top
+                );
+                ctx.lineWidth=2;
+                ctx.strokeStyle= getRGBA(weights[i][j]);
+                ctx.stroke();
+            }
+        }
         const nodeRadius=18;
         for(let i=0; i<inputs.length; i++){
-            const x=lerp(
-                left,
-                right,
-                inputs.length==1
-                    ? 0.5 : i/(inputs.length-1)
-            );
+            const x=Visualizer.#getNodeX(inputs,i,left,right);
             ctx.beginPath();
             ctx.arc(x,bottom,nodeRadius,0,Math.PI*2);
-            ctx.fillStyle="white";
+            ctx.fillStyle="black";
+            ctx.fill();
+            
+            ctx.beginPath();
+            ctx.arc(x,bottom,nodeRadius*0.6,0,Math.PI*2);
+            ctx.fillStyle=getRGBA(inputs[i]);
             ctx.fill();
         }
-        
         for(let i=0; i<outputs.length; i++){
-            const x=lerp(
-                left,
-                right,
-                outputs.length==1
-                    ? 0.5 : i/(outputs.length-1)
-            );
+            const x=Visualizer.#getNodeX(outputs,i,left,right);
             ctx.beginPath();
             ctx.arc(x,top,nodeRadius,0,Math.PI*2);
-            ctx.fillStyle="white";
+            ctx.fillStyle="black";
             ctx.fill();
+
+            ctx.beginPath();
+            ctx.arc(x,top,nodeRadius*0.6,0,Math.PI*2);
+            ctx.fillStyle=getRGBA(outputs[i]);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.lineWidth=2;
+            ctx.arc(x,top,nodeRadius*0.8,0,Math.PI*2);
+            ctx.strokeStyle = getRGBA(biases[i]);
+            ctx.setLineDash([3,3]);
+            ctx.stroke();
+            ctx.setLineDash([]);
+            
         }
+    }
+    
+
+    static #getNodeX(nodes,index,left,right){
+        return lerp(
+            left,
+            right,
+            nodes.length==1?0.5:index/(nodes.length-1)
+        )
     }
 }
