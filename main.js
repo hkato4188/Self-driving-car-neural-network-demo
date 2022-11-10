32e8f2ecdb7dc40ecfa4ea3b0b08c5370123b3af
hkato4188 <hirokikato1@gmail.com>
2022-11-10 11:37:01 -0500

    complete visualizer, create traffic, create mutate method

--- main.js
+++ main.js
@@ -9,14 +9,41 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
-const N=100;
+const N=30;
 const cars = generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+  for(let i=0; i<cars.length; i++){
+    cars[i].brain=JSON.parse(
+      localStorage.getItem("bestBrain")
+    );
+    if(i!=0){
+      NeuralNetwork.mutate(cars[i].brain,0.1);
+    }
+  }
+  
+}
 
 const traffic = [
-  new Car(road.getLaneCenter(1), -100, 20, 50, "DUMMY", 2)
+  new Car(road.getLaneCenter(1), -100, 20, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -300, 20, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 20, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -500, 20, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 20, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -700, 20, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -700, 20, 50, "DUMMY", 2)
 ];
 
 animate();
+
+function save(){
+  localStorage.setItem("bestBrain",
+  JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+  localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N){
   const cars=[];
@@ -35,7 +62,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find(
+  bestCar = cars.find(
     c=>c.y==Math.min(
       ...cars.map(c=>c.y)
     )
