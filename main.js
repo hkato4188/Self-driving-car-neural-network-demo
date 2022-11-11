88e120e019b4a072c3250fbf6f474c86ff296c44
hkato4188 <hirokikato1@gmail.com>
2022-11-11 16:42:01 -0500

    update mutate factor for neural network

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ if(localStorage.getItem("bestBrain")){
       localStorage.getItem("bestBrain")
     );
     if(i!=0){
-      NeuralNetwork.mutate(cars[i].brain,0.1);
+      NeuralNetwork.mutate(cars[i].brain,0.2);
     }
   }
   
